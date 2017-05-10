03c1a4713a96e028a2fa9fc1617d9a60a80e8c06
Nikola Uzelac <nikola@uzelac.io>
2017-05-10 01:22:30 -0700

    Update app.js

--- app.js
+++ app.js
@@ -8,3 +8,59 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var score, roundScore, activePlayer;
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 0;
+
+document.querySelector('.dice').style.display = 'none';
+
+document.querySelector('#score-0').textContent = '0';
+document.querySelector('#score-1').textContent = '0';
+document.querySelector('#current-0').textContent = '0';
+document.querySelector('#current-1').textContent = '0';
+
+
+
+function button(){
+    
+    var dice = Math.floor(Math.random() * 6) + 1;
+    
+    var diceDOM = document.querySelector('.dice');
+    
+    diceDOM.style.display = 'block';
+    
+    diceDOM.src = 'dice-' + dice + '.png';
+    
+    if (dice !== 1)
+        {
+            roundScore += dice;
+            
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
+        }
+    else
+        {
+            roundScore = 0;
+            
+            document.querySelector('current')
+            
+            activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+            
+            document.querySelector('#current-0').textContent = '0';
+            document.querySelector('#current-1').textContent = '0';
+            
+            document.querySelector('.player-0-panel').classList.toggle('active');
+            document.querySelector('.player-1-panel').classList.toggle('active');
+            
+            document.querySelector('.dice').style.display = 'none';
+        }
+}
+
+function hold(){
+    
+}
+
+document.querySelector('.btn-roll').addEventListener('click', button);
+document.querySelector('.btn-hold').addEventListener('click', hold);
